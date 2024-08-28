9711dbf6e84b55cabd29ae4eea283255fca71ae1
Eduardo Lemos <eduardolemosep@gmail.com>
2024-08-28 13:02:30 -0300

    server repo

---
+++ server/target/debug/deps/server-6e09ad393cff7095.d
@@ -0,0 +1,5 @@
+/Users/edu/Documents/headsafe/server/target/debug/deps/libserver-6e09ad393cff7095.rmeta: src/main.rs
+
+/Users/edu/Documents/headsafe/server/target/debug/deps/server-6e09ad393cff7095.d: src/main.rs
+
+src/main.rs:
