545c471b24cbad50fe86cc1ada8996422fefcd3b
Pedro Olyntho <66219757+pedroolynth0@users.noreply.github.com>
2024-09-03 17:20:02 -0300

    send image

--- embedded/main.py
+++ embedded/main.py
@@ -1,28 +1,82 @@
 from ultralytics import YOLO
+import cv2
+import time
+import json
+import base64
+import requests
+from datetime import datetime
+
+def save_and_send_image(image, server_url):
+    # Converte a imagem para o formato JPEG em memória e a codifica como binário
+    _, buffer = cv2.imencode('.jpg', image)
+
+    # Converte o numpy array para bytes
+    image_bytes = buffer.tobytes()
+
+    # Converte os bytes para uma string base64
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+
+    # Captura a data e hora atuais
+    date_taken = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Cria um JSON com a imagem em base64 e a data/hora de captura
+    json_data = json.dumps({
+        "image": image_base64,
+        "date_taken": date_taken
+    })
+
+    # Envia a imagem em uma solicitação POST para o servidor
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(server_url, data=json_data, headers=headers)
+
+    if response.status_code == 200:
+        print("Image and date successfully sent to the server.")
+    else:
+        print(f"Failed to send image and date. Status code: {response.status_code}")
+
+def capture_and_check_images(cap, model, server_url, num_photos=5, threshold=3):
+    detected_no_hardhats_count = 0
+    selected_image = None
+    
+    for _ in range(num_photos):
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to capture webcam's frame")
+            continue
+
+        results = model(frame)
+
+        for result in results:
+            for cls, _ in zip(result.boxes.cls, result.boxes.conf):
+                if int(cls) == 1:  # Índice da classe NO-Hardhats é 1
+                    detected_no_hardhats_count += 1
+                    selected_image = frame  # Armazena a última imagem detectada com NO-Hardhats
+                    break  # Não precisa continuar verificando as outras caixas
+
+        # Intervalo de 2 segundos entre cada captura
+        time.sleep(2)
+
+    if detected_no_hardhats_count >= threshold:
+        if selected_image is not None:
+            save_and_send_image(selected_image, server_url)
 
 if __name__ == "__main__":
-    # Carregar o modelo YOLOv8 pré-treinado
-    model = YOLO('yolov8n.pt')
+    model = YOLO('embedded/data/best.pt')
 
-    # Treinar o modelo utilizando a GPU 3060 Ti (especificando device=0)
-    results = model.train(
-        data='data.yaml',  # Caminho para o arquivo data.yaml
-        epochs=50,         # Número de épocas
-        batch=16,          # Tamanho do lote
-        imgsz=640,         # Tamanho das imagens
-        name='train_run34', # Nome da execução do treinamento
-        project='runs/detect', # Diretório onde os resultados serão salvos
-        workers=8,         # Número de processos de trabalho para carregar os dados
-        device=0,          # Especifica a GPU; '0' geralmente se refere à primeira GPU (3060 Ti)
-        amp=False          # Desabilitar AMP para evitar problemas
-    )
+    cap = cv2.VideoCapture(0)
 
-    # # Avaliar o modelo no conjunto de validação
-    # results = model.val(data='data.yaml')
+    if not cap.isOpened():
+        print("Failed to open webcam")
+        exit()
 
-    # Fazer inferências em uma nova imagem de teste
-    results = model('test/images/-19-_png_jpg.rf.f4aaa6a9746eea26e38e94d3c85ef10c.jpg')
-    results[0].show()  # Mostra a imagem com as detecções
+    server_url = "http://35.170.158.159:3030/"
 
-    # Salvar os resultados das inferências
-    results.save('runs/detect/inference_results/')
+
+    while True:
+        capture_and_check_images(cap, model, server_url)
+
+        # Aguarda 5 minutos (300 segundos) antes da próxima verificação
+        time.sleep(300)
+
+    cap.release()
+    cv2.destroyAllWindows()
