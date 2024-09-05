aee5eee27827c839ea43fb874378bce86580d6a1
Eduardo Lemos <eduardolemosep@gmail.com>
2024-09-05 16:41:38 -0300

    foo

--- server/src/client.rs
+++ server/src/client.rs
@@ -7,12 +7,19 @@ use tracing::{self, error, info};
 use crate::stg::Storage;
 use crate::image::*;
 
-#[derive(Clone, Deserialize)]
+#[derive(Clone, Deserialize, Debug)]
 pub struct LastRequest(pub String);
 
 #[derive(Clone)]
 pub struct HttpState {
     pub storage: Storage,
+}
+
+pub fn start_server(state: HttpState) -> Router {
+    Router::new()
+        .route("/", post(store_data))
+        .route("/hello", post(hello_handle))
+        .with_state(state)
 }
 
 pub async fn store_data(
@@ -33,13 +40,6 @@ pub async fn store_data(
     }
 }
 
-pub fn start_server(state: HttpState) -> Router {
-    Router::new()
-        .route("/", post(store_data))
-        .route("/hello", post(hello_handle))
-        .with_state(state)
-}
-
 /// The client will request connection to the server
 /// the sever will reply with new availiable images if there's any.
 pub async fn hello_handle(
@@ -47,6 +47,6 @@ pub async fn hello_handle(
     Json(last_req): axum::Json<LastRequest>,
     ) -> impl IntoResponse {
     info!("New subscriber joined");
-
+    info!("{:#?}", last_req);
     StatusCode::OK
 }
